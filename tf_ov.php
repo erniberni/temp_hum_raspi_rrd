54905c6c693f20d3a31dea3573110e811a5e5b0d
erniberni <rnickels@web.de>
2014-05-01 21:02:20 +0200

    Create tf_ov.php

---
+++ tf_ov.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+  <meta content="text/html; charset=ISO-8859-1"
+ http-equiv="content-type">
+  <title>DF Temperatur Feuchte Historie</title>
+</head>
+<body style="color: rgb(0, 0, 0); background-color: rgb(77, 77, 77);"
+ alink="#ee0000" link="#0000ee" vlink="#551a8b">
+<div style="text-align: center;"><span
+ style="color: white; font-family: DejaVu Sans;">DF Temperatur Feuchte Historie<br>
+<br>
+<IMG SRC="rrd.php?typ=temp&time=d&sensor=16" BORDER="0" ALT="ALT TEXT" WIDTH="" HEIGHT=""><br><br>
+<IMG SRC="rrd.php?typ=temp&time=w&sensor=16" BORDER="0" ALT="ALT TEXT" WIDTH="" HEIGHT=""><br><br>
+<IMG SRC="rrd.php?typ=temp&time=m&sensor=16" BORDER="0" ALT="ALT TEXT" WIDTH="" HEIGHT=""><br><br>
+<IMG SRC="rrd.php?typ=hum&time=d&sensor=16" BORDER="0" ALT="ALT TEXT" WIDTH="" HEIGHT=""><br><br>
+<IMG SRC="rrd.php?typ=hum&time=w&sensor=16" BORDER="0" ALT="ALT TEXT" WIDTH="" HEIGHT=""><br><br>
+<IMG SRC="rrd.php?typ=hum&time=m&sensor=16" BORDER="0" ALT="ALT TEXT" WIDTH="" HEIGHT=""><br>
+<IMG SRC="rrd.php?typ=batt&time=d&sensor=16" BORDER="0" ALT="ALT TEXT" WIDTH="" HEIGHT=""><br><br>
+<IMG SRC="rrd.php?typ=batt&time=w&sensor=16" BORDER="0" ALT="ALT TEXT" WIDTH="" HEIGHT=""><br><br>
+<IMG SRC="rrd.php?typ=batt&time=m&sensor=16" BORDER="0" ALT="ALT TEXT" WIDTH="" HEIGHT=""><br>
+</span></div>
+</body>
+</html>
