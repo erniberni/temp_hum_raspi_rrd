6e44a7e8b8abdd3ef27bc0834e8a1ca34a01e1ba
erniberni <rnickels@web.de>
2014-05-01 21:00:21 +0200

    Create tf.php

---
+++ tf.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html><head>
+
+<meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+<title>df raspi</title>
+</head>
+<body style="color: rgb(0, 0, 0); background-color: rgb(77, 77, 77)" alink="#ee0000" link="#0000ee" vlink="#551a8b">
+<div style="text-align: center;"><span style="color: rgb(255, 255, 255);">DF's RasPi<br></span>
+<br>
+<a href="./tf_ov.php">
+<img src="rrd.php?typ=temp&amp;time=d&amp;sensor=16" alt="ALT TEXT" border="0" height="" width=""></a><br>
+<br>
+<img src="rrd.php?typ=hum&amp;time=d&amp;sensor=16" alt="ALT TEXT" border="0" height="" width=""><br>
+<br>
+<img src="rrd.php?typ=batt&amp;time=d&amp;sensor=16" alt="ALT TEXT" border="0" height="" width=""><br>
+<br>
+</div>
+</body></html>
